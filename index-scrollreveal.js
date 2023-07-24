34edcf7a196e2319f384c33ba31b53e1276beb76
jesus pichardo <alepbuenrostro20@gmail.com>
2023-07-23 22:05:51 -0600

    To-Do List Javascript

---
+++ index-scrollreveal.js
@@ -0,0 +1,10 @@
+ScrollReveal().reveal(".todo-app", {
+	duration: 1800,
+	origin: "bottom",
+	distance: "-100px",
+});
+ScrollReveal().reveal(".info-autor", {
+	duration: 1800,
+	origin: "right",
+	distance: "-100px",
+});
